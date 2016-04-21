c131cc2efa6dbeac65e2ab71e5a44f496af7588e
Richard Silveira <richardleecba@gmail.com>
2016-04-21 17:40:29 -0400

    Creating Commands and Events

---
+++ PizzaApi/PizzaApi.MessageContracts/IRegisterOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApi.MessageContracts
+{
+    public interface IRegisterOrderCommand
+    {
+        string CustomerName { get; }
+        string CustomerPhone { get; }
+        int PizzaID { get; }
+    }
+}
---
+++ PizzaApi/PizzaApi.MessageContracts/IRejectOrderCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApi.MessageContracts
+{
+    interface IRejectOrderCommand
+    {
+    }
+}
+//public int OrderID { get; set; }
+
+//public string CustomerName { get; set; }
+
+//public string CustomerPhone { get; set; }
+
+//public int? EstimatedTime { get; set; }
+
+//public string StatusDescription { get; set; }
+
+//public string RejectedReasonPhrase { get; set; }
+
+//public int PizzaID { get; set; }
