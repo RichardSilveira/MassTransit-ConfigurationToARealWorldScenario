c704dd2265b13dd2a740a37a81bff243d39955c0
Richard Silveira <richardleecba@gmail.com>
2016-04-21 16:42:19 -0400

    Creating project structure according to the recommendation of the MassTransit documentation

---
+++ PizzaApi/PizzaApi.MessageContracts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApi.MessageContracts
+{
+    public class Class1
+    {
+    }
+}
